e3ecd03d23a408bb8f652308e6f0fb216f34464f
Emmanuel <emmanuel_12x@hotmail.com>
2019-09-23 16:49:31 -0500

    Carpetas Auth,layout agergadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './shared/auth/login/login.component';
+import { RegisterComponent } from './shared/auth/register/register.component';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
